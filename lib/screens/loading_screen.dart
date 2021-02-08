9ec00d8a856b4fe91bd3879e035c4be134987215
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-02-07 20:54:09 -0500

    Arrenged all the files according to the proper format

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  LoadingScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            //Get the current location
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
