98e4f0e74176ba4cc5f011e605ae53016c903198
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-02-09 21:48:30 -0500

    Added the method getDataByCoordinates. Imported the dart convert package. Retrieved the temperature, conditionNumber and cityName

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,7 @@
 // Packages:
 import 'package:flutter/material.dart';
-
-// import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 // Services:
 import 'package:clima/services/location.dart';
@@ -30,44 +29,34 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     print('Latitude: ${location.latitude} & Longitude: ${location.longitude}');
 
-    getCurrentData();
+    getDataByCoordinates(lat: location.latitude, long: location.longitude);
   }
 
-  void getCurrentData() async {
+  void getDataByCoordinates({double lat, double long}) async {
     String appID = '888e3a5d9c71ad9496ae5659aaf9bcc0';
-    String byCoordinatesURL = 'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$appID';
+    String byCoordinatesURL = 'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&appid=$appID';
     http.Response response = await http.get(byCoordinatesURL);
 
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+      // print(data);
+      // double longitude = jsonDecode(data)['coord']['lon'];
+      // print('Longitude: $longitude');
+      // String description = jsonDecode(data)['weather'][0]['description'];
+      // print('Description: $description');
+
+      dynamic decodedData = jsonDecode(data);
+      double temperature = decodedData['main']['temp'];
+      int conditionNumber = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+
+      // print('Temperature: $temperature');
+      // print('conditionNumber: $conditionNumber');
+      // print('cityName: $cityName');
     } else {
       print((response.statusCode));
     }
   }
-
-  Map weather = {
-    "coord": {"lon": -0.1257, "lat": 51.5085},
-    "weather": [
-      {"id": 501, "main": "Rain", "description": "moderate rain", "icon": "10n"}
-    ],
-    "base": "stations",
-    "main": {"temp": 272.88, "feels_like": 266.74, "temp_min": 272.04, "temp_max": 273.71, "pressure": 1009, "humidity": 74},
-    "visibility": 10000,
-    "wind": {"speed": 5.14, "deg": 50},
-    "rain": {"1h": 0.15},
-    "clouds": {"all": 75},
-    "dt": 1612919579,
-    "sys": {"type": 1, "id": 1414, "country": "GB", "sunrise": 1612941822, "sunset": 1612976755},
-    "timezone": 0,
-    "id": 2643743,
-    "name": "London",
-    "cod": 200
-  };
-
-  // Future<http.Response> fetchAlbum() {
-  //   return http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
-  // }
 
   @override
   Widget build(BuildContext context) {
