3da3c1713dc07f30b7b02c0a6c27d188e3dddf1a
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-02-11 18:43:32 -0500

    Modified lib/screens/location_screen.dart by adding the onPressed event on the nearby button

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -61,7 +61,10 @@ class _LocationScreenState extends State<LocationScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      var weatherData = await weatherHelper.getLocationWeather();
+                      updateUI(weatherData);
+                    },
                     child: Icon(
                       Icons.near_me,
                       size: 50.0,
