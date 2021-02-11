65739220e9210e94b3c4d97e6a126597e386439c
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-02-11 15:10:15 -0500

    Added an animated transition btween two screens

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -34,11 +34,15 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     // Adds an additional delay so we show a little more of the spinner:
     // await Future.delayed(const Duration(milliseconds: 500));
-    Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return LocationScreen(
-        locationWeatherData: weatherData,
-      );
-    }));
+    // Navigator.push(context, MaterialPageRoute(builder: (context) {
+    //   return LocationScreen(
+    //     locationWeatherData: weatherData,
+    //   );
+    // }));
+
+    // Using an animated transition between two screens:
+    // Navigator.push(context, _createRoute(destiny: LocationScreen(locationWeatherData: weatherData)));
+    Navigator.of(context).push(_createRoute(destiny: LocationScreen(locationWeatherData: weatherData)));
   }
 
   final spinkit = SpinKitDualRing(
@@ -54,4 +58,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
       child: spinkit,
     ));
   }
+}
+
+Route _createRoute({Widget destiny}) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => destiny,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
 }
