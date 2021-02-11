721ed9e82acdf8785c23bf11fa0313526557f7f2
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-02-11 01:30:06 -0500

    Added the flutter_spinkit package. Added a loading indicator  at screens/loading_screen.dart:39. Added route into the location_screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,13 @@
 // Packages:
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 // Services:
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
+
+// Screens:
+import 'package:clima/screens/location_screen.dart';
 
 class LoadingScreen extends StatefulWidget {
   LoadingScreen({Key key, this.title}) : super(key: key);
@@ -26,7 +30,27 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await location.getCurrentLocation();
     NetworkHelper network = NetworkHelper();
     var weatherData = await network.getDataByCoordinates(lat: location.latitude, long: location.longitude);
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
+
+  final spinkit = SpinKitFadingCircle(
+    itemBuilder: (BuildContext context, int index) {
+      return DecoratedBox(
+        decoration: BoxDecoration(
+          color: index.isEven ? Colors.red : Colors.green,
+        ),
+      );
+    },
+  );
+
+  // final spinkit = SpinKitSquareCircle(
+  //   color: Colors.white,
+  //   size: 50.0,
+  //   controller: AnimationController(vsync: TickerProvider., duration: const Duration(milliseconds: 1200)),
+  // );
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +59,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
           title: Text(widget.title),
         ),
         body: Container(
-          margin: EdgeInsets.all(30),
-          color: Colors.red,
+          // margin: EdgeInsets.all(30),
+          color: Colors.black,
+          child: spinkit,
         ));
   }
 }
