e139d4ada54abfae61e06c02f9846bd0de04ba17
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-02-14 01:23:08 -0500

    Added the package intl. Included gathering of utc date based in the received dt = timeSinceEpochInSec, conversion into local time and validation of the following times only, previoiusmto inert the column element into the list view

--- lib/components/horizontal_hourly_forecast_card_child.dart
+++ lib/components/horizontal_hourly_forecast_card_child.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:deep_pick/deep_pick.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
 
 // Components:
 import 'package:clima/components/reusable_card.dart';
@@ -49,37 +50,58 @@ class HorizontalHourlyForecastCardChild extends StatelessWidget {
     );
     result.add(presentWeather);
 
-    // currentTemperature = pick(weatherData, 'main', 'temp').asDoubleOrNull() ?? -100;
-    // currentMinTemperature = pick(weatherData, 'main', 'temp_min').asDoubleOrNull() ?? -100;
-    // currentMaxTemperature = pick(weatherData, 'main', 'temp_max').asDoubleOrNull() ?? -100;
-    // currentConditionNumber = pick(weatherData, 'weather', 0, 'id').asIntOrNull() ?? 0;
-    // cityName = pick(weatherData, 'name').asStringOrNull() ?? 'Unknown'; // * Not included on One Call
-    // currentIconCode = pick(weatherData, 'weather', 0, 'icon').asStringOrNull() ?? '11n';
-
-    // print(hourlyForecast.value[0]);
-
     for (var i = 0; i <= 24; i++) {
-      // print(pick(hourlyForecast[i], 'dt').asDoubleOrNull());
-      // print(hourlyForecast);
-      Container newtWeatherElement = Container(
-        padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text('${pick(hourlyForecast[i], 'dt').asDoubleOrNull()}'),
-            Image(
-              image: weatherHelper.getOpenWeatherSmallIcon(iconCode: pick(hourlyForecast[i], 'weather', 0, 'icon').asStringOrNull()),
-              width: 40,
-            ),
-            Text('${pick(hourlyForecast[i], 'temp').asDoubleOrNull().toStringAsFixed(1) ?? 0} °'), // hourly[0].temp
-          ],
-        ),
-      );
+      var timeSinceEpochInSec = pick(hourlyForecast[i], 'dt').asIntOrNull();
+      // print('timeSinceEpochInSec: $timeSinceEpochInSec');
 
-      result.add(newtWeatherElement);
+      final dateUtc = DateTime.fromMillisecondsSinceEpoch(timeSinceEpochInSec * 1000, isUtc: true);
+      // print('dateUtc: $dateUtc');
+
+      var newDateLocal = dateUtc.toLocal();
+      // print('newDateLocal: $newDateLocal');
+
+      DateTime now = new DateTime.now();
+      DateTime upToHourDateTime = new DateTime(now.year, now.month, now.day, now.hour);
+      // print(upToHourDateTime);
+
+      if (newDateLocal.isAfter(upToHourDateTime)) {
+        // print('Compliant newDateLocal: $newDateLocal');
+
+        // var formattedDateTime = DateFormat('hh:mm a').format(newDateLocal);
+        var formattedDateTime = DateFormat('ha').format(newDateLocal).toLowerCase();
+        // print('formattedDateTime: $formattedDateTime');
+
+        // Composing the element to insert
+        Container newtWeatherElement = Container(
+          padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('$formattedDateTime'),
+              Image(
+                image: weatherHelper.getOpenWeatherSmallIcon(iconCode: pick(hourlyForecast[i], 'weather', 0, 'icon').asStringOrNull()),
+                width: 40,
+              ),
+              Text('${pick(hourlyForecast[i], 'temp').asDoubleOrNull().toStringAsFixed(1) ?? 0} °'), // hourly[0].temp
+            ],
+          ),
+        );
+
+        result.add(newtWeatherElement);
+      }
     }
 
     return result;
+  }
+
+  String getClockInUtc(int timeSinceEpochInSec) {
+    final time = DateTime.fromMillisecondsSinceEpoch(timeSinceEpochInSec * 1000, isUtc: true);
+    return '${time.hour}:${time.second}';
+  }
+
+  String getClockInUtcPlus3Hours(int timeSinceEpochInSec) {
+    final time = DateTime.fromMillisecondsSinceEpoch(timeSinceEpochInSec * 1000, isUtc: true).add(const Duration(hours: 3));
+    return '${time.hour}:${time.second}';
   }
 
   @override
